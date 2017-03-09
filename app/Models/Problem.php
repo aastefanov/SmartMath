6cd57eab4303e2d8c1a646e7f7d2936093786815
Albert Stefanov <aastefanov@outlook.com>
2017-03-10 00:23:31 +0200

    Added Hints Support
    Created Hint model and migration
    Added hasMany relationship to Problem for hints

--- app/Models/Problem.php
+++ app/Models/Problem.php
@@ -8,19 +8,24 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Problem extends Model
 {
-    use SoftDeletes;
+	use SoftDeletes;
 
-    protected $fillable = ['description', 'answer', 'category_id', 'difficulty'];
+	protected $fillable = ['description', 'answer', 'category_id', 'difficulty'];
 
-    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+	protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
-    public function users()
-    {
-        return $this->belongsToMany('App\User')->withPivot('is_correct');
-    }
+	public function users()
+	{
+		return $this->belongsToMany('App\User')->withPivot('is_correct');
+	}
 
-    public function category()
-    {
-        return $this->belongsTo('App\Models\Category');
-    }
+	public function category()
+	{
+		return $this->belongsTo('App\Models\Category');
+	}
+
+	public function hint()
+	{
+		return $this->hasMany('App\Models\Hint');
+	}
 }

52eb1e8acbea9005effd5db036dcd5a5518c1fe5
Albert Stefanov <aastefanov@outlook.com>
2017-03-09 21:12:56 +0200

    added migrate route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | by your application. Just tell Laravel the URIs it should respond
 | to using a Closure or controller method. Build something great!
 |
-*/
+ */
 
 // Dashboard and auth
 Route::get('/', 'IndexController@index');
@@ -18,26 +18,31 @@ Auth::routes();
 
 // User
 Route::group([
-    'middleware' => 'auth'
+	'middleware' => 'auth'
 ], function () {
-    Route::get('/profile', 'ProfileController@index');
-    Route::get('/category/{categoryId}', 'CategoriesController@getProblem');
-    Route::post('/problems/solve/{problemId}', 'CategoriesController@solve');
+	Route::get('/profile', 'ProfileController@index');
+	Route::get('/category/{categoryId}', 'CategoriesController@getProblem');
+	Route::post('/problems/solve/{problemId}', 'CategoriesController@solve');
 });
 
 // Admin panel
 Route::group([
-    'middleware' => ['auth', 'admin'],
-    'namespace' => 'Admin',
-    'prefix' => 'admin'
+	'middleware' => ['auth', 'admin'],
+	'namespace' => 'Admin',
+	'prefix' => 'admin'
 ],function () {
-    Route::get('/', 'IndexController@index');
-    Route::resource('/users', 'UsersController');
-    Route::resource('/categories', 'CategoriesController');
-    Route::get('/problems/{id}', 'ProblemsController@show');
-    Route::get('/problems/{id}/edit', 'ProblemsController@edit');
-    Route::get('/problems/create/{categoryId}', 'ProblemsController@createFromCategory');
-    Route::post('/problems', 'ProblemsController@store');
-    Route::put('/problems/{id}', 'ProblemsController@update');
-    Route::delete('/problems/{id}', 'ProblemsController@delete');
+	Route::get('/', 'IndexController@index');
+	Route::resource('/users', 'UsersController');
+	Route::resource('/categories', 'CategoriesController');
+	Route::get('/problems/{id}', 'ProblemsController@show');
+	Route::get('/problems/{id}/edit', 'ProblemsController@edit');
+	Route::get('/problems/create/{categoryId}', 'ProblemsController@createFromCategory');
+	Route::post('/problems', 'ProblemsController@store');
+	Route::put('/problems/{id}', 'ProblemsController@update');
+	Route::delete('/problems/{id}', 'ProblemsController@delete');
 });
+
+Route::get('/migrate', function() {
+	Artisan::call('migrate');
+});
+
